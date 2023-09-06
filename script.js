43cf9d71ff4b8d0fe8e849a19ccf0042bd216f84
RudiJr <rudi.juniior@gmail.com>
2023-09-06 17:32:57 -0300

    music button now plays music, changed pomodoro buttons and created the changes for titles

--- script.js
+++ script.js
@@ -2,15 +2,64 @@ const html = document.querySelector('html');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play();
+    }else { 
+        musica.pause();
+    };
+});
 
 focoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
+    alterarContexto('foco');
+    focoBtn.classList.add('active');
 });
 
 curtoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
+    alterarContexto('descanso-curto');
+    curtoBtn.classList.add('active');
 }); 
 
 longoBtn.addEventListener('click', ()=> {
-    html.setAttribute('data-contexto', 'descanso-longo');
+    alterarContexto('descanso-longo');
+    longoBtn.classList.add('active');
 });
+
+function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    });
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`); // template string
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br> 
+                                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+        case "descanso-longo": 
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+                                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            break;
+    
+        default:
+            break;
+    };
+};
+
+/* `` is a template string in JavaScript. It is
+    used to dynamically insert the value of the variable
+    `contexto` into the string. In this case, it is used to
+    dynamically generate the source path for the `banner` image
+    based on the value of the `contexto` variable. */
